5c193d2dce14f6bad3182c0b058d5a4ad97ae48a
Cedric Batailler <cedric.batailler@gmail.com>
2018-02-28 16:16:36 +0100

    coloring

--- experiment.js
+++ experiment.js
@@ -802,6 +802,8 @@ var iat_block_2 = {
     {
       type: 'iat-html',
       stimulus: jsPsych.timelineVariable('stimulus'),
+      category: jsPsych.timelineVariable('category'),
+      label_category: ['maths-arts'],
       stim_key_association: jsPsych.timelineVariable('stim_key_association'),
       html_when_wrong: '<span style="color: red; font-size: 80px">&times;</span>',
       bottom_instructions: '<p>If you press the wrong key, a red &times; will appear. Press the other key to continue</p>',
@@ -945,6 +947,8 @@ var iat_block_3_test = {
     {
       type: 'iat-html',
       stimulus: jsPsych.timelineVariable('stimulus'),
+      category: jsPsych.timelineVariable('category'),
+      label_category: ['self-other', 'maths-arts'],
       stim_key_association: jsPsych.timelineVariable('stim_key_association'),
       html_when_wrong: '<span style="color: red; font-size: 80px">X</span>',
       bottom_instructions: '<p>If you press the wrong key, a red X will appear. Press the other key to continue</p>',
